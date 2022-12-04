943d916d52912b05247d80c04df73b428fd30c35
Panks <pankajsingla012@gmail.com>
2022-12-04 13:18:12 +0530

    Section 10 Part 1 : Adding data to Firebase

--- src/contexts/products-context.js
+++ src/contexts/products-context.js
@@ -1,13 +1,19 @@
-import { createContext, useState } from "react";
-import PRODUCTS from "../shop-data.json";
+import { createContext, useState, useEffect } from "react";
+import SHOP_DATA from "../shop-data";
+
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductsContext = createContext({
     products : []
 });
 
 export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
     const value = {products};
+
+    useEffect(()=>{
+        addCollectionAndDocuments('categories', SHOP_DATA);
+    }, []);
 
     return (
         <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -14,7 +14,9 @@ import {
     getFirestore,
     doc,
     getDoc,
-    setDoc
+    setDoc,
+    collection,
+    writeBatch
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -40,6 +42,20 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    Array.from(objectsToAdd).forEach((object) => {
+        const docRef = doc(collectionRef, object.title.toLowerCase());
+        batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log("done");
+}
+
 export const createUserDocumentFromAuth = async (
     userAuth, 
     additionalInformation = {displayName: 'name'}) => {

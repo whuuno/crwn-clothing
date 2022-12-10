5f0b1c085f58cd7fc7e388817e602c0608f8c9c5
Panks <pankajsingla012@gmail.com>
2022-12-10 14:41:08 +0530

    Section 13 Part 1 : Understanding Reducer & Adding UserReducer

--- src/contexts/user-context.jsx
+++ src/contexts/user-context.jsx
@@ -1,4 +1,4 @@
-import {createContext, useState, useEffect} from 'react';
+import {createContext, useEffect, useReducer} from 'react';
 
 import {createUserDocumentFromAuth, onAuthStateChangeListener, signOutUser} from '../utils/firebase/firebase.utils'
 
@@ -8,8 +8,37 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 })
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER : 'SET_CURRENT_USER',
+}
+
+export const userReducer = (state, action) => {
+    console.log('dispatch');
+    console.log(action);
+    const { type, payload } = action;
+     
+    switch (type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER : 
+            return {
+                ...state,
+                currentUser : payload
+            }
+        default :
+            throw new Error(`Unhandled Type ${type} in Reducers`)
+    }
+}
+
+export const INITIAL_STATE = {
+    currentUser : null
+}
+
+
 export const UserProvider = ({children}) =>{
-    const [currentUser, setCurrentUser] = useState(null);
+    const [ { currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    //const currentUser = state;
+    const setCurrentUser = (user) => {
+        dispatch({ type : USER_ACTION_TYPES.SET_CURRENT_USER, payload : user});
+    }
     const value = { currentUser, setCurrentUser };
 
     useEffect(()=>{
